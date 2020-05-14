e3d081ba11b9c1ec335018fe79a7ff27ad93aec4
Tyo2685 <deny.prasetyo@students.amikom.ac.id>
2020-05-14 13:48:15 +0700

    Konversi Ke Json,Xml dan Csv

---
+++ Konversi_JsonXmlCsv/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Konversi_JsonXmlCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.npm     = "19.11.2685";
+            mhs.nama    = "Deny Agung Prasetyo";
+            mhs.email   = "deny.prasetyo@students.amikom.ac.id";
+
+            Console.WriteLine("Pilih Format Konversi Data : ");
+            Console.WriteLine("1. Json");
+            Console.WriteLine("2. XML");
+            Console.WriteLine("1. Csv\n");
+
+            Console.Write("Nomor Format Data [1-3] : ");
+            int nomorFormatData = Convert.ToInt32(Console.ReadLine());
+
+            IConvertObject convert; //deklarasi objek konversi
+
+            if(nomorFormatData == 1)
+                convert = new ConvertToJson();
+            else if(nomorFormatData == 2)
+                convert = new ConvertToXml();
+            else
+                convert = new ConvertToCsv();
+
+            //konversi objek mhs berdasarkan format yang dipilih 
+            convert.Convert(mhs);
+
+            Console.ReadKey();
+
+        }
+    }
+}
